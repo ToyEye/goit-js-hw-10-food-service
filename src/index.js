b2b0fcb9e146b0bb91bb333b2969d2146aff6f3c
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-10-07 16:43:38 +0300

    theme

--- src/index.js
+++ src/index.js
@@ -10,3 +10,27 @@ menuRef.insertAdjacentHTML('beforeend', cardsMarkUp);
 function createMarkUpMenu(menu) {
     return cardsTemplates(menu);
 }
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodeRef = document.body;
+bodeRef.classList.add('light-theme');
+
+document.addEventListener('change', classToggle);
+
+function classToggle(e) {
+    const checkBox = document.querySelector('.theme-switch__toggle');
+    if (bodeRef.classList.contains('light-theme')) {
+        bodeRef.classList.remove('light-theme');
+        bodeRef.classList.add('dark-theme');
+        checkBox.getAttribute('checked');
+        localStorage
+    }
+    else if (bodeRef.classList.contains('dark-theme')) {
+        bodeRef.classList.remove('dark-theme');
+        bodeRef.classList.add('light-theme');
+    }  
+    }
