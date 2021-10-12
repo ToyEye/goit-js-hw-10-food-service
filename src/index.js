defbcf70b6acfd1c3df5516698560c5020453dea
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-10-12 15:47:50 +0300

    checkbox

--- src/index.js
+++ src/index.js
@@ -5,45 +5,51 @@ import cardsTemplates from "./templates/carsd.hbs";
 
 const menuRef = document.querySelector('.js-menu');
 const cardsMarkUp = createMarkUpMenu(menu);
-menuRef.insertAdjacentHTML('beforeend', cardsMarkUp);
 
-function createMarkUpMenu(menu) {
-    return cardsTemplates(menu);
-}
+const bodeRef = document.body;
+const checkBox = document.querySelector('.theme-switch__toggle');
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-const bodeRef = document.body;
-const checkBox = document.querySelector('.theme-switch__toggle');
-console.log(checkBox);
+
+menuRef.insertAdjacentHTML('beforeend', cardsMarkUp);
+
+function createMarkUpMenu(menu) {
+    return cardsTemplates(menu);
+}
+
 
 document.addEventListener('change', classToggle);
 
 themeDefault();
+getLS();
 
 function classToggle(e) {
     e.preventDefault();
-
     bodeRef.classList.toggle(Theme.DARK);
 
     if (bodeRef.classList.contains(Theme.DARK)) {
         localStorage.setItem('theme', Theme.DARK);
         localStorage.setItem('checkbox', checkBox.checked);
-
     }
     else {
         let checkBoxFalse = checkBox.checked = false;
-    localStorage.setItem('theme', Theme.LIGHT);
-    localStorage.setItem('checkbox', checkBoxFalse);}
+        localStorage.setItem('theme', Theme.LIGHT);
+        localStorage.setItem('checkbox', checkBoxFalse);
+    }
 }
 
 function themeDefault() {
     if (!localStorage.length) {
         localStorage.setItem('theme', Theme.LIGHT);
     }
+}
+    
+
+function getLS() {
     bodeRef.classList.add(localStorage.getItem('theme'));
-    localStorage.getItem('checkbox');
-   }   
+    checkBox.setAttribute('checked', localStorage.getItem('checkbox'));
+ }  
