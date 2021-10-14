cd9e754537e71610554a3d9ec8ac899ec00b219b
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-10-14 20:57:19 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import menu from "./menu.json";
-import cardsTemplates from "./templates/carsd.hbs";
-
-
+import menu from './menu.json';
+import cardsTemplates from './templates/carsd.hbs';
 
 const menuRef = document.querySelector('.js-menu');
 const cardsMarkUp = createMarkUpMenu(menu);
@@ -14,43 +12,36 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-
 menuRef.insertAdjacentHTML('beforeend', cardsMarkUp);
 
 function createMarkUpMenu(menu) {
-    return cardsTemplates(menu);
+  return cardsTemplates(menu);
 }
-
 
 document.addEventListener('change', classToggle);
 
 themeDefault();
 
 if (bodyRef.classList.value === Theme.DARK) {
-    checkBox.checked = true;
+  checkBox.checked = true;
 }
 
 function classToggle(e) {
-   
-
-    if (e.target.checked) {
-        bodyRef.classList.add(Theme.DARK);
-        bodyRef.classList.remove(Theme.LIGHT);
-        localStorage.setItem('theme', Theme.DARK);
-    }
-    else {
-        bodyRef.classList.add(Theme.LIGHT);
-        bodyRef.classList.remove(Theme.DARK);
-        localStorage.setItem('theme', Theme.LIGHT);
-    }
+  if (e.target.checked) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyRef.classList.add(Theme.LIGHT);
+    bodyRef.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
 }
 
 function themeDefault() {
-    if (localStorage === null) {
-        bodyRef.classList.add(Theme.LIGHT);
-    } else {
-        bodyRef.classList.add(localStorage.getItem('theme'));
-    }
+  if (localStorage === null) {
+    bodyRef.classList.add(Theme.LIGHT);
+  } else {
+    bodyRef.classList.add(localStorage.getItem('theme'));
+  }
 }
-    
-
